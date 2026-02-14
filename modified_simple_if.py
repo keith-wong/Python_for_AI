520940043501089f6a1ed2008601799444188ecd
KeithWong <kwongchiu66@gmail.com>
2026-02-15 02:28:50 +0800

    add modified if statement

---
+++ modified_simple_if.py
@@ -0,0 +1,17 @@
+x=5
+
+print("before 5")
+if x==5:
+	print("is 5!")
+	print("5 second time!")
+	print("5 third time!")
+
+x=6
+print("before 6!")
+if x==6:
+	print("is 6!")
+	print("is 6 scond time !")
+	print("is 6 thirs time !")
+
+print ("all Done!")
+
