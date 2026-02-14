56fe89b99a1e779745afdc4f0f4b10f458b88128
KeithWong <kwongchiu66@gmail.com>
2026-02-15 04:27:18 +0800

    add multiway decision code to repo

---
+++ multiway_decision.py
@@ -0,0 +1,15 @@
+print("in this program you'll be asked for a number not more than 100 input and compare with fixed values")
+
+x = input ("please enter an integer!\n")
+
+x = int(x)
+
+if x<2:
+	print("small")
+elif x<50:
+	print ("lager")
+else:
+	print ("HUGE")
+
+print("DONE")
+
