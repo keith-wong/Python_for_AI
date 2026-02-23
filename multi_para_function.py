053e3af36cf1be7152eeaa2f32067d07e72aae8b
KeithWong <kwongchiu66@gmail.com>
2026-02-24 03:54:00 +0800

    python multi parameter function

---
+++ multi_para_function.py
@@ -0,0 +1,12 @@
+def add_two (a, b):
+	sum = a + b
+	return round(sum, 3)
+
+num1 = float(input("enter a number:\n"))
+num2 = float(input("enter another number :\n")) 
+
+#add_two(num1, num2)
+
+print(f"your result is {add_two(num1, num2)} ")
+
+
