09721508d4e448095d3f53046c96c4c010b9858c
KeithWong <kwongchiu66@gmail.com>
2026-02-24 04:07:42 +0800

    modfied

--- multi_para_function.py
+++ multi_para_function.py
@@ -3,7 +3,7 @@ def add_two (a, b):
 	return round(sum, 3)
 
 num1 = float(input("enter a number:\n"))
-num2 = float(input("enter another number :\n")) 
+num2 = float(input("enter another number :\n"))
 
 #add_two(num1, num2)
 
