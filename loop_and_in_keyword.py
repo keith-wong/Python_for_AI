2454a4bfdd516cc647a21b8df54c1990c092ac23
KeithWong <kwongchiu66@gmail.com>
2026-02-26 15:04:41 +0800

    loop a list of number with in keyword

---
+++ loop_and_in_keyword.py
@@ -0,0 +1,13 @@
+# print a tring before looping
+
+print  ("before looping !")
+
+for  item in [9, 41, 12, 3, 74, 6]:
+	print (item)
+
+# print  a string afterlooping
+
+print ("after looping!")
+
+
+
