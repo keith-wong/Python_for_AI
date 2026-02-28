e4eb19039f558c20662abbe2e676eeac87e91d02
KeithWong <kwongchiu66@gmail.com>
2026-02-28 15:10:33 +0800

    sum all values in a list

---
+++ summation_in_loop.py
@@ -0,0 +1,11 @@
+sum = 0 #create variable  sum
+print("Before sum: ", sum)
+
+for item in [9, 41, 12, 3, 74, 15]:
+	sum = sum + item
+	print(item,  sum)
+
+print("after summation , the value is : ", sum)
+
+
+
