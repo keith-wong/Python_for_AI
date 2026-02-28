76c914556d07a0d891952dfe9ab8db71819614ce
KeithWong <kwongchiu66@gmail.com>
2026-02-28 18:20:44 +0800

    find average in a list of umbers

---
+++ find_average_in_list.py
@@ -0,0 +1,18 @@
+# this program is to find the average in a list of numbers
+
+count = 0
+sum = 0
+print ("Before average : ", count, sum)
+
+for value in [9, 41, 12, 3, 74, 15]:
+	count =  count + 1
+	sum = sum + value
+	print (count,sum, value)
+
+x = round (sum / count, 2)
+ 
+
+print (f"after average of {count} items, total of {sum}, average is {x}")
+
+
+
