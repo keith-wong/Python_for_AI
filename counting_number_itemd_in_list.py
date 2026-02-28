0f326e48e29475ab83fcc0148de7cad17ef3d754
KeithWong <kwongchiu66@gmail.com>
2026-02-28 14:54:08 +0800

    counting number of items in a list

---
+++ counting_number_itemd_in_list.py
@@ -0,0 +1,11 @@
+count = 0 # create counter 
+
+print("Before counting number of count is: ", count)
+
+#looping code block
+
+for item in [9, 41, 12, 3, 74, 15]:
+	count = count + 1
+	print (count, item)
+
+print ("After counting number of count is: ", count )
