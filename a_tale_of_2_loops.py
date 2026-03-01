41554df18f3487b184d7c29985defcce910b5eef
KeithWong <kwongchiu66@gmail.com>
2026-03-02 04:37:08 +0800

    add 2 files

---
+++ a_tale_of_2_loops.py
@@ -0,0 +1,18 @@
+# loop 1
+
+friends = ['joeseph', 'glenn', 'sally']
+
+for friend in friends:
+	print("happy birthday", friend)
+
+print ("all done")
+
+# loop 2 index the position of list
+
+for k  in range( len(friends)):
+	friend = friends[k]
+	print("happy birthday", friend)
+
+print ("all done")
+
+ 
---
+++ append_in_list.py
@@ -0,0 +1,8 @@
+stuff = list()
+
+stuff.append('book')
+stuff.append('pencil')
+stuff.append('rubber')
+print (stuff)
+
+
